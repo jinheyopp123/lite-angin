8da4904c57b74d8689396b9b4357552d11c4007c
jinheyopp123 <161195769+jinheyopp123@users.noreply.github.com>
2024-04-21 19:07:49 +0900

    권한 원할 접속을 위한 파일

--- permissions.js
+++ permissions.js
@@ -1,1 +1,20 @@
+// permissions.js
 
+// 권한 확인 함수
+function checkPermission(req, res, next) {
+    if (!req.session.isAdmin) {
+        res.status(403).send('권한이 부족합니다.');
+    } else {
+        next();
+    }
+}
+
+// 권한 업데이트 함수
+function updatePermission(req, res, next) {
+    // 차단된 사용자에 대한 ACL 접근 가능하도록 업데이트
+    req.session.canAccessBlockedUsers = true;
+    
+    next();
+}
+
+module.exports = { checkPermission, updatePermission };
